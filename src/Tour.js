3d8c20a032e1f0b7ba631e1d461f4b25446c368b
Daniel Soots <danielsoots@icloud.com>
2022-01-12 11:06:32 -0500

    added show less/more function for tour info paragraph

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Tour = ({ name, info, price, image }) => {
+    const [readMore, setReadMore] = useState(false);
+
     return (
         <article className="single-tour">
             <img src={image} alt={name} />
@@ -9,7 +11,12 @@ const Tour = ({ name, info, price, image }) => {
                     <h4>{name}</h4>
                     <h4 className="tour-price">${price}</h4>
                 </div>
-                <p>{info}</p>
+                <p>
+                    {readMore ? info : `${info.substring(0, 200)}...`}
+                    <button onClick={() => setReadMore(!readMore)}>
+                        {readMore ? `Read Less` : "Read More"}
+                    </button>
+                </p>
                 <button className="delete-btn">Not interested</button>
             </footer>
         </article>
