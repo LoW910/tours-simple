582613ab4e56ca362dc01fece9c3360f31612ba6
Daniel Soots <danielsoots@icloud.com>
2022-01-12 10:58:32 -0500

    moved tour data from fetch to tour/s component added not interested btn to opt out of tour later

--- src/Tours.js
+++ src/Tours.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Tour from "./Tour";
 const Tours = ({ tours }) => {
-    const { id, name, info, image, price } = tours;
-
-    return;
+    return (
+        <>
+            <section>
+                <div className="title">
+                    <h2>Our Tours</h2>
+                    <div className="underline"></div>
+                </div>
+                {tours.map((tour) => {
+                    return <Tour key={tour.id} {...tour} />;
+                })}
+            </section>
+        </>
+    );
 };
 
 export default Tours;
